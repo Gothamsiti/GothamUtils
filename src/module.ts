abc2f3aa3ae6948038064345618e758067df98d0
Umberto <umberto@gotham.studio>
2026-05-28 15:01:11 +0200

    chore: pre-release changes

--- src/module.ts
+++ src/module.ts
@@ -1,7 +1,4 @@
-import { readdirSync } from 'node:fs'
-import { dirname, join } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import { defineNuxtModule, addPlugin, createResolver, addServerHandler, addImportsDir } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver, addImportsDir } from '@nuxt/kit'
 import type { Resolver } from '@nuxt/kit'
 
 // Module options TypeScript interface definition
@@ -30,29 +27,10 @@ export interface ModuleOptions {
   cloudflare: CloudFlareOptions | undefined
   iubenda: IubendaOptions | undefined
 }
-const currentDir = dirname(fileURLToPath(import.meta.url))
 
-const addServerRoutes = (resolver: Resolver) => {
-  // tutto quello che segue è stato fatto perche le dynamic route come da documentazione non funzionano.
-  const basePath = '/api/storyblok'
-  const sourcePath = './runtime/server/api/storyblok'
-  const targetDir = join(currentDir, sourcePath)
-  const files = readdirSync(targetDir)
-  const cleanedFiles = files.filter(e => e.includes('.js') || e.includes('.ts')).map(e => e.replace('.js', '').replace('.ts', ''))
-  cleanedFiles.forEach((handler) => {
-    const [route, method] = handler.split('.')
-    addServerHandler({
-      route: `${basePath}/${route}`,
-      handler: resolver.resolve(`${sourcePath}/${route}.${method}`),
-    })
-  })
-}
 const addPlugins = (resolver: Resolver) => {
   addPlugin(resolver.resolve('./runtime/plugins/01.setupLanguages'))
   addPlugin(resolver.resolve('./runtime/plugins/02.utils'))
-  addPlugin(resolver.resolve('./runtime/plugins/03.labels'))
-  addPlugin(resolver.resolve('./runtime/plugins/04.images'))
-  addPlugin(resolver.resolve('./runtime/plugins/05.datasources'))
 }
 const addComposables = (resolver: Resolver) => {
   addImportsDir(resolver.resolve('runtime/composables'))
@@ -76,7 +54,6 @@ export default defineNuxtModule<ModuleOptions>({
     _nuxt.options.runtimeConfig.public.gothamstoryblok = { storyblok: { version: _options.storyblok?.version || 'draft' } }
     const resolver = createResolver(import.meta.url)
     addPlugins(resolver)
-    addServerRoutes(resolver)
     addComposables(resolver)
   },
 })
--- src/runtime/middleware/languageRedirect.global.js
+++ src/runtime/middleware/languageRedirect.global.js
@@ -1,13 +1,14 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    const { defaultLanguage, languages } = useLanguage();
+import { defineNuxtRouteMiddleware, useLanguage, navigateTo } from '#imports'
 
-    const paths = to.fullPath.replace(/^\/+/, '').split('/');
-    if(to.fullPath.includes('/api')) return;
-    const sentToCorrectUrl = (lang) => {
-        return navigateTo(`/${lang}${to.fullPath}`);
-    }
-    if(paths.length < 1 || paths[0].length != 2 || !languages.value.includes(paths[0])){
-        return sentToCorrectUrl(defaultLanguage.value);
-    }
-  });
-  
+export default defineNuxtRouteMiddleware((to) => {
+  const { defaultLanguage, languages } = useLanguage()
+
+  const paths = to.fullPath.replace(/^\/+/, '').split('/')
+  if (to.fullPath.includes('/api')) return
+  const sentToCorrectUrl = (lang) => {
+    return navigateTo(`/${lang}${to.fullPath}`)
+  }
+  if (paths.length < 1 || paths[0].length != 2 || !languages.value.includes(paths[0])) {
+    return sentToCorrectUrl(defaultLanguage.value)
+  }
+})
