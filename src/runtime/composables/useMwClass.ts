abe66af6dcd00169441d6ad7fd64e00b3a7ade72
Roberto Silvestri <roberto@gotham.studio>
2026-09-25 10:42:40 +0200

    chore: pre-release changes

--- src/runtime/composables/useMwClass.ts
+++ src/runtime/composables/useMwClass.ts
@@ -31,6 +31,10 @@ const SIDE_ALIASES: Record<string, string> = {
 
 const BORDER_NONE_RE = /^border-(top|right|bottom|left)-none$/
 
+const FLEX_GROWN_RE = /^flex-grown$/
+
+const NO_STICKY_RE = /^no-sticky$/
+
 type ClassField
   = | string
     | string[]
@@ -249,6 +253,30 @@ function parseToken(
       value: 'none',
       className: `border-${direction}-none`,
       property: `border-${direction}`,
+      type: 'literal',
+    }
+  }
+
+  const flexGrown = token.match(FLEX_GROWN_RE)
+
+  if (flexGrown) {
+    return {
+      variable: '',
+      value: '1',
+      className: 'has-flex-grown',
+      property: 'flex',
+      type: 'literal',
+    }
+  }
+
+  const noSticky = token.match(NO_STICKY_RE)
+
+  if (noSticky) {
+    return {
+      variable: '',
+      value: 'static',
+      className: 'has-no-sticky',
+      property: 'position',
       type: 'literal',
     }
   }
